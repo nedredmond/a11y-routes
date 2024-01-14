2ab90e1654f46867cb7d60f587eedbf93fe63f0d
Ned Redmond <nedredmond@pm.me>
2024-01-13 21:47:11 -0500

    fetch test

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { RouteObject } from "react-router-dom";
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import type { WcagResponseData } from "./types";
@@ -7,39 +7,7 @@ import { WcagContext } from "./contexts/wcag-context";
 import loader from "./loaders/wcag-loader";
 import { parse } from "./utils";
 import { Text } from "./components/text";
-
-// const wcagRoutes = (wcag: WcagResponseData): RouteObject[] => [
-//   {
-//     path: "wcag2",
-//     loader: () => loader(),
-//   },
-//   ...wcag.principles
-//     .map((principle): RouteObject[] =>
-//       pathAliases.map((pKey) => ({
-//         path: `wcag2/${parse(principle[pKey])}`,
-//         loader: () => loader(principle),
-//         children: principle.guidelines
-//           .map((guideline): RouteObject[] =>
-//             pathAliases.map((gKey) => ({
-//               path: `wcag2/${parse(principle[pKey])}/${parse(guideline[gKey])}`,
-//               loader: () => loader(guideline),
-//               children: guideline.successcriteria
-//                 .map((successcriterion): RouteObject[] =>
-//                   pathAliases.map((scKey) => ({
-//                     path: `wcag2/${parse(principle[pKey])}/${parse(
-//                       guideline[gKey],
-//                     )}/${parse(successcriterion[scKey])}`,
-//                     loader: () => loader(successcriterion),
-//                   })),
-//                 )
-//                 .flat(),
-//             })),
-//           )
-//           .flat(),
-//       })),
-//     )
-//     .flat(),
-// ];
+import getWcag from "./w3c/wcag/get-wcag";
 
 const wcagRoutes = (wcag: WcagResponseData): RouteObject[] => [
   {
@@ -97,6 +65,11 @@ const router = (data: WcagResponseData) =>
 
 const Router = () => {
   const data = React.useContext(WcagContext);
+  useEffect(() => {
+    getWcag().then((data) => {
+      console.log(data);
+    })
+  }, []);
   return <RouterProvider router={router(data)} />;
 };
 
---
+++ src/w3c/wcag/get-wcag.ts
@@ -0,0 +1,11 @@
+import type { WcagResponseData } from "../../types";
+
+export default async (): Promise<WcagResponseData> =>
+  await fetch(
+    "https://raw.githubusercontent.com/w3c/wcag/main/guidelines/wcag.json",
+  ).then(async (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error("Failed to fetch WCAG data");
+  });
