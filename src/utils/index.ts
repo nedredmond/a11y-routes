05abb237bb230d3c48560739ca02442271ee5485
Ned Redmond <nedredmond@pm.me>
2024-01-15 11:11:11 -0500

    restore parser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,13 @@
-export const parseNum = (num: string) => num.split(".").pop() as string;
-
 export const parse = (segment: string) => {
   if (segment.includes(".") || !isNaN(Number(segment))) {
-    return parseNum(segment);
+    // return last segment of a number
+    // e.g., "1.2.3" -> "3"
+    return segment.split(".").pop() as string;
+  }
+  if (segment.includes(":")) {
+    // return last segment of a colon-separated string
+    // e.g., "WCAG2:robust" -> "robust"
+    return segment.split(":").pop() as string;
   }
   return segment;
 };
