c07ea62184b9e99682b16121f3b524953460e66e
Ned Redmond <nedredmond@pm.me>
2024-01-15 00:25:31 -0500

    fix up styles

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Text = ({ children }: React.PropsWithChildren) => (
   <span
     style={{
-      fontSize: "xxx-large",
+      fontSize: "large",
     }}
   >
     {children}
